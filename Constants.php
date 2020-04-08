ffa825d04d4e4521584ddbef5b3d3c934fe09368
Manal <mmsaj000@hotmail.com>
2020-04-08 22:10:02 +0300

    Update Constants.php

--- Constants.php
+++ Constants.php
@@ -1,9 +1,8 @@
 <?php
-    define('DB_NAME','alisa1_bus1');//alisa1_bus
-    define('DB_USER','alisa1');
-    define('DB_PASSWORD','u3)08p+P5TYtgO');
-    //smartbus1234
-    define('DB_HOST','localhost');
+    define('DB_NAME','root');
+    define('DB_USER','db');
+    define('DB_PASSWORD','');
+  define('DB_HOST','localhost');
 
 
     	?>
